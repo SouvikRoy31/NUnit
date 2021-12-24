99287a50842e4118e796d6b1cee2a90926f86981
souvikroy <souvik.roy0331@outlook.com>
2021-12-24 22:56:20 +0530

    init commit

---
+++ Selenium/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    class UnitTest1
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        
+
+        [Test]
+        public void Test1()
+        {
+            driver.Url = "https://www.duckduckgo.com";
+            IWebElement searchbar = driver.FindElement(By.Name("q"));
+            IWebElement searchbutton = driver.FindElement(By.Id("search_button_homepage"));
+            searchbar.SendKeys("LambdaTest");
+            searchbar.Submit();
+            Assert.Pass();
+        }
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+
+    }
+}
